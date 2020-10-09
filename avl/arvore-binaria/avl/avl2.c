80a6e94a9be8fb1ada3d2125b5c9a8575be9c004
Emilio Calvet <38714959+emiliocalvet@users.noreply.github.com>
2020-10-09 15:26:16 -0300

    first commit

--- avl/arvore-binaria/avl/avl2.c
+++ avl/arvore-binaria/avl/avl2.c
@@ -7,52 +7,19 @@ typedef struct no
     struct no *esq, *dir;
 } No;
 
-inserir(int x, No *pt, int h);
-rot_direita(No *pt, int h);
-rot_esquerda(No *pt, int h);
+void inserir(int x, No *pt, int h);
+void rot_direita(No *pt, int h);
+void rot_esquerda(No *pt, int h);
 
 int qtd_no = 0;
 
 int main()
 {
+    No *pt = NULL;
     return 0;
 }
 
-rot_direita(No *pt, int h) {
-    No *ptv = NULL;
-    No *ptu = pt->esq;
-
-    if (ptu->bal == -1)
-    {
-        pt->esq = ptu->dir;
-        ptu->dir = pt;
-        pt = ptu;
-        pt->bal = pt->dir->bal = 0;
-    }
-    else
-    {
-        ptv = ptu->dir;
-        ptu->dir = ptv->esq;
-        pt->esq = ptv->dir;
-        ptv->esq = ptu;
-        ptv->dir = pt;
-        pt = ptv;
-        if (ptv->bal == -1)
-        {
-            ptu->bal = 0;
-            pt->dir->bal = 1;
-        }
-        else
-        {
-            ptu->bal = -1;
-            pt->dir->bal = 0;
-        }
-    }
-}
-
-rot_esquerda(No *pt, int h) {}
-
-inserir(int x, No *pt, int h)
+void inserir(int x, No *pt, int h)
 {
     if (pt == NULL)
     {
@@ -122,6 +89,114 @@ inserir(int x, No *pt, int h)
                 }
             }
         }
+    }
+}
+
+void rot_direita(No *pt, int h)
+{
+    No *ptv = NULL;
+    No *ptu = pt->esq;
+
+    if (ptu->bal == -1)
+    {
+        pt->esq = ptu->dir;
+        ptu->dir = pt;
+        pt = ptu;
+        pt->bal = pt->dir->bal = 0;
+    }
+    else
+    {
+        ptv = ptu->dir;
+        ptu->dir = ptv->esq;
+        pt->esq = ptv->dir;
+        ptv->esq = ptu;
+        ptv->dir = pt;
+        pt = ptv;
+        if (ptv->bal == -1)
+        {
+            ptu->bal = 0;
+            pt->dir->bal = 1;
+        }
+        else
+        {
+            ptu->bal = -1;
+            pt->dir->bal = 0;
+        }
+    }
+}
+
+void rot_esquerda(No *pt, int h)
+{
+    No *ptv = NULL;
+    No *ptu = pt->dir;
+
+    if (ptu->bal == 1)
+    {
+        pt->dir = ptu->esq;
+        ptu->esq = pt;
+        pt = ptu;
+        pt->bal = pt->esq->bal = 0;
+    }
+    else
+    {
+        ptv = ptu->esq;
+        ptu->esq = ptv->dir;
+        pt->dir = ptv->esq;
+        ptv->dir = ptu;
+        ptv->esq = pt;
+        pt = ptv;
+        if (ptv->bal == 1)
+        {
+            ptu->bal = 0;
+            pt->esq->bal = -1;
+        }
+        else
+        {
+            ptu->bal = 1;
+            pt->esq->bal = 0;
+        }
+    }
+}
+
+void pre_ordem(No *aux)
+{
+    if (aux)
+    {
+        printf("No: %d(bal:%d) ", aux->chave, aux->bal);
+        pre_ordem(aux->esq);
+        pre_ordem(aux->dir);
+    }
+    else
+    {
+        printf("Arvore vazia!");
+    }
+}
+
+void in_ordem(No *aux)
+{
+    if (aux)
+    {
+        in_ordem(aux->esq);
+        printf("No: %d(bal:%d) ", aux->chave, aux->bal);
+        in_ordem(aux->dir);
+    }
+    else
+    {
+        printf("Arvore vazia!");
+    }
+}
+
+void pos_ordem(No *aux)
+{
+    if (aux)
+    {
+        pos_ordem(aux->esq);
+        pos_ordem(aux->dir);
+        printf("No: %d(bal:%d) ", aux->chave, aux->bal);
+    }
+    else
+    {
+        printf("Arvore vazia!");
     }
 }
 
